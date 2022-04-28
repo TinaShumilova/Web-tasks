0edd681e05f8cfcc216780605cee8137192a6b71
Tina Shumilova <shutiva@gmail.com>
2022-04-28 11:15:41 +0300

    add button and script

---
+++ Scripts/script.js
@@ -0,0 +1,25 @@
+function testButton() {
+    alert("Тестовая кнопочка");
+    let confirmation = confirm("Сложим числа?");
+        if (confirmation == true){
+            let answer = parseInt(prompt ("Сколько будет 2+2 ?"));
+            switch (answer) {
+                case 4:
+                    alert("Молодчинка!");
+                    break;
+                case 3:
+                    alert("Маловато будет!");
+                    break;
+                case 5:
+                    alert("А чего так мтого?!");
+                    break;
+                default:
+                    alert("Совсем не правильно :(")
+                    break;
+            }
+        } else {
+            alert("Ну как хочешь...")
+        }
+
+
+}
